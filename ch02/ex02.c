6ce86948b51a0395a490ac9f573a7371350a6a2b
scholarly-punk <169328705+scholarly-punk@users.noreply.github.com>
2024-12-30 16:58:09 +1100

    Parkinson's Law

---
+++ ch02/ex02.c
@@ -0,0 +1,18 @@
+#include <stdio.h> // Directive
+
+int main(void)
+{
+  printf("Parkinson's Law:\nWork expands so as to "); // Statement
+  printf("fill the time\n"); // Statement
+  printf("available for its completion.\n"); // Statement
+  
+  return 0; // Statement
+
+  /*
+    Program Output:
+
+    Parkinson's Law:
+    Work expands so as to fill the time
+    available for its completion.
+  */
+}
