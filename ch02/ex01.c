7a85e30e4b1302d5c069621e3195c1680f4df2a8
scholarly-punk <169328705+scholarly-punk@users.noreply.github.com>
2024-12-30 16:52:06 +1100

    Answered question in the comments.

--- ch02/ex01.c
+++ ch02/ex01.c
@@ -3,4 +3,17 @@
 int main(void)
 {
   printf("hello, world\n");
+
+  /*
+    No I didn't get a warning from the compiler, when compiled with
+
+    gcc -march=native -O3 -std=c99 -W -Wall -Wextra -Werror -pedantic
+    -o ex01 ex01.c
+    
+    But I did when I added the -ansi flag.
+   */
+
+  return 0;
+
+  /* The above got rid of the warning even with the -ansi flag. */  
 }
