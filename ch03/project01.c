dc6636e48d7ce6c83d19f09700ed3023f780011b
scholarly-punk <169328705+scholarly-punk@users.noreply.github.com>
2024-12-31 20:29:38 +1100

    Date formatting

---
+++ ch03/project01.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+  printf("Enter a date (mm/dd/yyyy): ");
+  int month, day, year;
+  scanf("%d/%d/%d", &month, &day, &year);
+
+  printf("You entered the date %04d%02d%02d\n", year, month, day);
+  
+  return 0;
+}
