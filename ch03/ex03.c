a799717a9f4ae3ccdfbc18b65754ee5dd2495868
scholarly-punk <169328705+scholarly-punk@users.noreply.github.com>
2024-12-31 20:08:49 +1100

    scanfs

---
+++ ch03/ex03.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int x;
+  // a
+  scanf("%d", &x);
+  printf("%d\n", x);
+
+  // b
+  scanf(" %d", &x);
+  printf("%d\n", x);
+
+  // a and b equivalent
+
+  int y, z;
+  // c
+  scanf("%d-%d-%d", &x, &y, &z);
+  printf("%d %d %d\n", x, y, z);
+
+  // d
+  scanf("%d -%d -%d", &x, &y, &z);
+  printf("%d %d %d\n", x, y, z);
+
+  // c and d equivalent
+
+  float a;
+
+  // e
+  scanf("%f", &a);
+  printf("%f\n", a);
+
+  // f
+  scanf("%f ", &a);
+  printf("%f\n", a);
+
+  // e and f not equivalent, f needs a space entered after the number.
+
+  float b;
+  // g
+  scanf("%f,%f", &a, &b);
+  printf("%f %f\n", a, b);
+
+  // h
+  scanf("%f, %f", &a, &b);
+  printf("%f %f\n", a, b);
+  
+  return 0;
+}
