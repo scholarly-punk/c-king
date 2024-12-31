d347531f3f97adbec4e70c26bccd2509144e61f5
scholarly-punk <169328705+scholarly-punk@users.noreply.github.com>
2024-12-31 20:19:20 +1100

    Another strange scanf

---
+++ ch03/ex05.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+  float x, y;
+  int i;
+  scanf("%f%d%f", &x, &i, &y);
+  printf("%f %d %f\n", x, i, y);
+
+  // Output 12.3 45 0.600000
+  
+  return 0;
+}
