f1003e53516351d937c596c7dafba0b0ee5f4f49
scholarly-punk <169328705+scholarly-punk@users.noreply.github.com>
2024-12-31 20:54:54 +1100

    Scanf limiter

--- ch03/project01.c
+++ ch03/project01.c
@@ -4,7 +4,7 @@ int main(void)
 {
   printf("Enter a date (mm/dd/yyyy): ");
   int month, day, year;
-  scanf("%d/%d/%d", &month, &day, &year);
+  scanf("%2d/%2d/%4d", &month, &day, &year);
 
   printf("You entered the date %04d%02d%02d\n", year, month, day);
   
