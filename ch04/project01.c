b05dfde6a9d227f2b13a0000a5f38cce953bf3f6
scholarly-punk <169328705+scholarly-punk@users.noreply.github.com>
2025-01-02 12:25:13 +1100

    Reverse 2 digits

---
+++ ch04/project01.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+  printf("Enter a two-digit number: ");
+  int n;
+  scanf("%d", &n);
+
+  int d1 = n / 10;
+  int d2 = n % 10;
+
+  printf("The reversal is: %d%d\n", d2, d1);
+  
+  return 0;
+}
